d9bcdad483fdcf9dfe46fb32562e0cd6a43d0eba
Lexx YungCarter <spencermainaa@gmail.com>
2021-05-13 02:26:52 +0300

    feat: added sidebar/drawer

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,28 +1,58 @@
 <template>
   <ion-page>
-    <ion-header>
+    <ion-header :translucent="true">
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary"></ion-menu-button>
+        </ion-buttons>
         <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <ion-list>
+        <ion-item v-for="i in 30" :key="i">
+          <ion-title>{{ i }}</ion-title>
+          <ion-text>Number</ion-text>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonText,
+  IonButtons,
+  IonMenuButton,
+} from "@ionic/vue";
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default {
+  name: "Tab1",
+  components: { 
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonPage,
+    IonList,
+    IonItem,
+    IonText,
+    IonButtons,
+    IonMenuButton,
+  },
+};
 </script>
